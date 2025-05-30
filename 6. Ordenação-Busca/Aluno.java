1d72d2be2cf0988186092d03a7c9f65141c2e803
Guismx <guilhermesmx@gmail.com>
2025-05-30 01:30:32 -0300

    fix: ajustando atributos e adicionado alguns métoods

--- 6. Ordenação-Busca/Aluno.java
+++ 6. Ordenação-Busca/Aluno.java
@@ -1,47 +1,44 @@
-import java.util.List;
-
 public class Aluno {
     private String nome;
-    private int numero_matricula;
-    private Turma nome_turma;
+    private int numeroMatricula;
+    private Turma turma;
     private int periodo;
-    private List<Nota> notas;
+    private Nota[] notas = new Nota[20];
+    private int qtdNotas = 0;
 
-    public Aluno(){
+    public Aluno(String nome, int numeroMatricula, Turma turma, int periodo) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+        this.turma = turma;
+        this.periodo = periodo;
     }
 
-    public Aluno(String nome, int numero_matricula, Turma nome_turma, int periodo) {
-        this.nome = nome;
-        this.numero_matricula = numero_matricula;
-        this.nome_turma = nome_turma;
-        this.periodo = periodo;
+    public void adicionarNota(Nota nota) {
+        if (qtdNotas >= notas.length) {
+            Nota[] novoArray = new Nota[notas.length * 2];
+            for (int i = 0; i < notas.length; i++) novoArray[i] = notas[i];
+            notas = novoArray;
+        }
+        notas[qtdNotas++] = nota;
+    }
+
+    public Nota[] getNotas() {
+        return notas;
     }
 
     public String getNome() {
         return nome;
     }
-    public void setNome(String nome) {
-        this.nome = nome;
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
     }
 
-    public int getNumero_matricula() {
-        return numero_matricula;
-    }
-    public void setNumero_matricula(int numero_matricula) {
-        this.numero_matricula = numero_matricula;
-    }
-
-    public Turma getNome_turma() {
-        return nome_turma;
-    }
-    public void setNome_turma(Turma nome_turma) {
-        this.nome_turma = nome_turma;
+    public Turma getTurma() {
+        return turma;
     }
 
     public int getPeriodo() {
         return periodo;
-    }
-    public void setPeriodo(int periodo) {
-        this.periodo = periodo;
     }
 }
